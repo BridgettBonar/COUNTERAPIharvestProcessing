7886f6d98c63d5812e9bf9a8753ec1f0046371f0
BridgettBonar <140545755+BridgettBonar@users.noreply.github.com>
2025-01-27 10:19:51 -0500

    Update COUNTERAPIharvest_simple.R

--- COUNTERAPIharvest_simple.R
+++ COUNTERAPIharvest_simple.R
@@ -1,2 +1,34 @@
 library(tidyverse)
 
+#Import Job report as "SUSHI_Month"
+#Change "_Jan" below to the current month
+SUSHI <- SUSHI_Jan %>%
+#Rename columns
+  rename("Interface" = "...1") %>%
+  rename("Report" = "...2") %>%
+  rename("Date" = "...3") %>%
+  rename("Result" = "...4") %>%
+#Simplify Interface and report names
+  separate(Interface, sep = " - ", into = c("Interface", NA)) %>%
+  separate(Report, sep = ":", into = c("Report", NA)) 
+
+#pulls completed reports
+completed <- SUSHI %>%
+  filter(str_starts(Result, pattern = "Completed")) 
+
+#Pulls failed reports
+failed <- SUSHI %>%
+  filter(str_detect(Result, "Failed - Server"))
+
+#removes completed and failed reports from full list  
+nodata <- anti_join(SUSHI, completed) %>%
+  anti_join(failed)
+
+library(openxlsx)
+#create list of tables
+list = list("Failed" = failed,
+     "noData" = nodata,
+     "Complete" = completed)
+#export tables to Excel
+write.xlsx(list, "SUSHIharvest_2025January.xlsx")
+
